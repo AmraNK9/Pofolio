a8f7574c5b33852a82c82f05b46cc7e4b88dfc6d
AmaraNK9 <naymyo1310mdy@gmail.com>
2023-09-20 11:29:32 +0630

    add

--- app.js
+++ app.js
@@ -14,9 +14,34 @@ document.querySelectorAll(".navButton").forEach(
             e.target.classList.add("active")
         }
           else{
-            console.log(         e.target.parentElement);
             e.target.parentElement.classList.add("active")
           }
         })
     }
 )
+
+let isOpen = true
+//config
+if(window.screen.width < 600){
+    isOpen = false
+  }else{
+    document.querySelector(".fa-bars").classList.add("rotate")
+    isOpen = true
+  }
+document.querySelector(".fa-bars").addEventListener("click", function() {
+    document.querySelector(".fa-bars").classList.toggle("rotate")
+
+    if(isOpen){
+
+    document.querySelector(".container").style.marginLeft = "20px"
+    document.querySelector(".sideNav").style.marginLeft = "-100px"
+    document.querySelector(".fa-bars").style.marginLeft = "10px"
+    isOpen = false
+    }else{
+        document.querySelector(".container").style.marginLeft = "120px"
+        document.querySelector(".sideNav").style.marginLeft = "0px"
+        document.querySelector(".fa-bars").style.marginLeft = "100px"
+        isOpen = true
+    }
+  });
+  
